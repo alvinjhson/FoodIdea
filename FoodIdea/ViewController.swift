bb0c10135e7a4ac455856e9a9ff3279f77f19c27
alvinjhson <alvinbest26@gmail.com>
2024-03-19 12:36:41 +0100

    Initial Commit

---
+++ FoodIdea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodIdea
+//
+//  Created by Alvin Johansson on 2024-03-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
