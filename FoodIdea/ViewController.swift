a8b5b37fb75631280efb552d678c3df5e2c9e308
alvinjhson <alvinbest26@gmail.com>
2024-03-21 12:23:22 +0100

    Version 1

--- FoodIdea/ViewController.swift
+++ FoodIdea/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let segueId = "goToSecondSegue"
+    var food = AddFoodViewController()
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var foodType: UILabel!
+    @IBOutlet weak var contry: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func addRecipeButton(_ sender: UIButton) {
+        //performSegue(withIdentifier:segueId, sender: self)
+    }
+    
+    @IBAction func randomFoodButton(_ sender: Any) {
+            if let randomFood = food.getRandomFood() {
+                print("Hittade mat: \(randomFood.description)")
+                desc.text = randomFood.description
+                foodType.text = randomFood.foodType
+                contry.text = randomFood.country
+            } else {
+                print("Ingen mat hittades!")
+            }
+        }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueId {
+            if let destinationVC = segue.destination as? AddFoodViewController{
+                
+            
+                
+            }
+        }
+    }
+    
 }
 
